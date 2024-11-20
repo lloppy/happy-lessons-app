223f9256b36adf0699c2c811da477f92f5939bb9
lloppy <ankudinovapol@gmail.com>
2024-11-20 17:26:34 +0500

    add dagger hilt

---
+++ app/src/main/java/com/lloppy/hapson/app/di/DataModule.kt
@@ -0,0 +1,10 @@
+package com.lloppy.hapson.app.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+}
---
+++ app/src/main/java/com/lloppy/hapson/app/di/DomainModule.kt
@@ -0,0 +1,10 @@
+package com.lloppy.hapson.app.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class DomainModule {
+}
